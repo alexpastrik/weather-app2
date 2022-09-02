e9347fde146133e6d5d4ca0c9ac6beeca4724acf
alexpastrik <102768664+alexpastrik@users.noreply.github.com>
2022-09-02 13:31:39 -0400

    added github link

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <Weather />
+
+      <p>
+        This project was coded by ALex Pastrik and it is{" "}
+        <a href="https://github.com/alexpastrik/weather-app2" target="_blank">
+          open-sourced.
+        </a>
+      </p>
     </div>
   );
 }
